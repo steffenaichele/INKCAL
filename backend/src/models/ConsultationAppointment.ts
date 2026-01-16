416e1533319dad35dd66df17035fcb0ef5412365
Steffen Aichele <steffen.aichele@icloud.com>
2026-01-16 16:43:31 +0100

    added contact types to apointment

--- backend/src/models/ConsultationAppointment.ts
+++ backend/src/models/ConsultationAppointment.ts
@@ -1,8 +1,21 @@
 import { Schema } from "mongoose";
 import Appointment from "./Appointment.ts";
 
+const ContactSchema = new Schema(
+	{
+		type: {
+			type: String,
+			required: true,
+			enum: ["Instagram", "WhatsApp", "Email"],
+		},
+		details: { type: String, required: true },
+	},
+	{ _id: false }
+);
+
 const ConsultationSchema = new Schema({
-    clientName: { type: String, required: true },
+	clientName: { type: String, required: true },
+	contact: { type: ContactSchema, required: true },
 });
 
 export default Appointment.discriminator("Consultation", ConsultationSchema);
--- backend/src/models/NewTattooAppointment.ts
+++ backend/src/models/NewTattooAppointment.ts
@@ -1,13 +1,25 @@
 import { Schema } from "mongoose";
 import Appointment from "./Appointment.ts";
 
+const ContactSchema = new Schema(
+	{
+		type: {
+			type: String,
+			required: true,
+			enum: ["Instagram", "WhatsApp", "Email"],
+		},
+		details: { type: String, required: true },
+	},
+	{ _id: false }
+);
+
 const NewTattooSchema = new Schema({
-    clientName: { type: String, required: true },
-    designDescription: { type: String, required: true },
-    placement: { type: String},
-    size: { type: String},
-    color: { type: Boolean },
+	clientName: { type: String, required: true },
+	designDescription: { type: String, required: true },
+	placement: { type: String, required: false },
+	size: { type: String, required: false },
+	color: { type: Boolean, required: false },
+    contact: { type: ContactSchema, required: true },
 });
 
 export default Appointment.discriminator("NewTattoo", NewTattooSchema);
-
