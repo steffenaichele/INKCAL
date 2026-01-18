296d259fcc326eee20d8b1f3a6856a0c7c981361
Steffen Aichele <steffen.aichele@icloud.com>
2026-01-18 11:33:17 +0100

    Add bcrypt and jsonwebtoken dependencies; update Home component to display user list

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/query-client";
-import "index.css";
+import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,23 @@
+import { useUsers } from "../hooks/useUsers";
+
 const Home = () => {
-    return (
-        <h1>Home Page</h1>
-    );
+	const { data: users, isLoading, error } = useUsers();
+
+	if (isLoading) return <div>Lädt Users...</div>;
+	if (error) return <div>Fehler: {error.message}</div>;
+
+	return (
+		<div>
+			<h1>User Liste</h1>
+			<ul>
+				{users?.map((user) => (
+					<li key={user._id}>
+						{user.name} - {user.email}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default Home;
